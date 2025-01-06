7219437767fc7c0485027037499b67f7da46e44a
amirmub <amirmubarek01@gmail.com>
2025-01-06 17:35:31 +0300

    added a media query for responsive

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.getElementById("container")
 const overlayCon = document.getElementById("overlayCon")
 const overlayBtn = document.getElementById("overlayBtn");
 
+
 overlayBtn.addEventListener("click",() => {
   container.classList.toggle("right-panel-active");
 
